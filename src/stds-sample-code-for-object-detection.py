6ffea89772f7d6c43cf1404858974cae23cb4f72
GiselleP <pachecog2706@gmail.com>
2023-04-21 17:34:18 -0600

    x,y,z plot in 3d added

--- src/stds-sample-code-for-object-detection.py
+++ src/stds-sample-code-for-object-detection.py
@@ -11,6 +11,9 @@ from __future__ import print_function
 import cv2 
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
 
 
 # ------------------------- Define utility functions ----------------------- #
@@ -75,6 +78,15 @@ high_H_name = 'High H'
 high_S_name = 'High S'
 high_V_name = 'High V'
 
+#declaring arrays
+distance_in_x=list()
+distance_in_y=list()
+distance_in_z=list()
+measurements=list()
+measures_o_center_in_x=list()
+measures_o_center_in_y=list()
+measures_o_center_in_z=list()
+
 
 # ------------------ Parse data from the command line terminal ------------- #
 parser = argparse.ArgumentParser(description='Vision-based object detection')
@@ -89,36 +101,103 @@ cap = cv2.VideoCapture(args.video_file)
 cv2.namedWindow(window_capture_name, cv2.WINDOW_NORMAL)
 cv2.namedWindow(window_detection_name,cv2.WINDOW_NORMAL)
 
+#define start time of the video (18 seg) in miliseconds
+start_time=18
+cap.set(cv2.CAP_PROP_POS_MSEC, start_time * 1000)
 
-# ------------ Configure trackbars for the low and hight HSV values -------- #
-cv2.createTrackbar(low_H_name, window_detection_name , low_H, max_value_H, on_low_H_thresh_trackbar)
-cv2.createTrackbar(high_H_name, window_detection_name , high_H, max_value_H, on_high_H_thresh_trackbar)
-cv2.createTrackbar(low_S_name, window_detection_name , low_S, max_value, on_low_S_thresh_trackbar)
-cv2.createTrackbar(high_S_name, window_detection_name , high_S, max_value, on_high_S_thresh_trackbar)
-cv2.createTrackbar(low_V_name, window_detection_name , low_V, max_value, on_low_V_thresh_trackbar)
-cv2.createTrackbar(high_V_name, window_detection_name , high_V, max_value, on_high_V_thresh_trackbar)
+#define end time (2:57, 177 seg) in miliseconds
+end_time=177
+
+frame_rate = cap.get(cv2.CAP_PROP_FPS)
+frames_selected_time=int((end_time-start_time)*frame_rate)
+
+x_list=[]
+y_list=[]
+z_list=[]
+cx=1920
+cy=1080
+f=1723.72
+Z=50
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+def get_real_coordinates(x_obtained,y_obtained):
+        coor_x,coor_y=x_obtained,y_obtained
+
+        u=coor_x-cx
+        v=cy-coor_y
+
+        x_global=float((u/f)*Z)
+        y_global=float((-v/f)*Z)  
+        z_global=50
+
+        return x_global,y_global,z_global
+    
+def plot_x_y_z(x_global,y_global,z_global):
+    ax.plot(x_global, y_global, z_global,marker='o',color='blue')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.pause(0.1)
+
+def get_acumulated_coordinates(x_global,y_global,z_global):
+    x_list.append(x_global)
+    y_list.append(y_global)
+    z_list.append(z_global)
+
+    return x_list,y_list,z_list
+
+def draw_rectangle(contour):
+    perimeter=cv2.arcLength(contour,True)
+    polynomio=cv2.approxPolyDP(contour,0.02*perimeter,True)
+    x_,y_,w,h=cv2.boundingRect(polynomio)
+    
+    return x_,y_,w,h
+
+
+
+
+# def get_distance_between_points(x_list,y_list,z_list):
+#     #Append all the coordinates based on the center in lists x and y separately
+#     distance_in_x=x_list
+#     distance_in_y=y_list
+#     distance_in_z=z_list
+
+#     substraction_iteration=np.arange(0,frames_selected_time-1,-1)
+
+#     print(substraction_iteration)
+    # if len(distance_in_x)==1:
+    #Create a for loop to  substract the measurements and get the distance   
+        # for j in substraction_iteration:
+        #         # print(len(distance_in_x))
+        #     substraction_in_x=distance_in_x[j]-distance_in_x[j-1]
+        #     measures_o_center_in_x.append(substraction_in_x)
+            
+#         substraction_in_y=distance_in_y[j]-distance_in_y[j-1]
+#         measures_o_center_in_y.append(substraction_in_y)
+
+#         substraction_in_z=distance_in_z[j]-distance_in_z[j-1]
+#         measures_o_center_in_z.append(substraction_in_z)
+
+# #Create a for loop to calculare the magnitude
+#     for h in range(frames_selected_time):
+#         magnitude=np.sqrt(np.square(measures_o_center_in_x[h-1])+np.square(measures_o_center_in_y[h-1]))
+#         measurements.append(magnitude)
+    
+    # return measures_o_center_in_x
+
+
 
 # ------- Main loop --------------- #
-while True:
-
+for frames in range(frames_selected_time):
     # Read current frame
     ret, frame = cap.read()
 
     # Check if the image was correctly captured
     if frame is None:
         break
-
-    
-    # # Resize current frame
-    # width = int(frame.shape[1] * args.frame_resize_percentage / 100)
-    # height = int(frame.shape[0] * args.frame_resize_percentage / 100)
-    # dim = (width, height)
-    # frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-
-    # Apply the median filter
-    frame=cv2.medianBlur(frame,3)
-
-    
+   
     # Convert the current frame from BGR to HSV
     frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
@@ -128,28 +207,35 @@ while True:
     # Convert the current frame from BGR to Gray
     framem = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    
-    # Filter out the grassy region from current frame and keep the moving object only
-    # bitwise_AND = cv2.bitwise_and(frame_HSV, frame_HSV, mask=frame_threshold)
+        
+    # Filter out the grassy re+gion from current frame and keep the moving object only
     bitwise_AND = cv2.bitwise_and(frame, frame, mask=frame_threshold)
     contours, hierarchy = cv2.findContours(frame_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
     for contour in contours:
         area=cv2.contourArea(contour)
-        # print(area)
         max_value=np.max(bitwise_AND[:,:,1])
-        # if max_value>100 and max_value<215:
-        if area>420:   
-            perimeter=cv2.arcLength(contour,True)
-            polynomio=cv2.approxPolyDP(contour,0.02*perimeter,True)
-            # print(len(polynomio))
-            x_,y_,w,h=cv2.boundingRect(polynomio)
+        if area>420:
+            ax.clear
+            x_,y_,w,h=draw_rectangle(contour)
             cv2.rectangle(bitwise_AND,(x_-40,y_-40),(x_+100,y_+100),(0,255,0),1)
 
-    # Visualise both the input video and the object detection windows
-    # Create a new window for visualisation purposess
-    cv2.imshow(window_capture_name, frame)
+            if frames % (30) == 0:
+                x_global,y_global,z_global=get_real_coordinates(x_,y_)
+                x_list,y_list,z_list=get_acumulated_coordinates(x_global,y_global,z_global)
+                plot_x_y_z(x_list,y_list,z_list)
+            
+                if len(x_list)>=2:
+                    #getting the measurement in x
+                    substraction_in_x=x_list[-1]-x_list[-2]
+                    measures_o_center_in_x.append(substraction_in_x)
+                    print(measures_o_center_in_x)
+                
+                
 
+    # Visualise both the input video and the object detection windows
+    # Create a new window for visualisation purposes
+    cv2.imshow(window_capture_name, frame)
     cv2.imshow(window_detection_name, bitwise_AND)
 
     # The program finishes if the key 'q' is pressed
