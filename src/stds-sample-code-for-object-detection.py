e1a17211547cc43ebbac585a47a38d5e2bed2a9c
GiselleP <pachecog2706@gmail.com>
2023-04-22 00:26:00 -0600

    Final modified code

--- src/stds-sample-code-for-object-detection.py
+++ src/stds-sample-code-for-object-detection.py
@@ -109,9 +109,11 @@ cap.set(cv2.CAP_PROP_POS_MSEC, start_time * 1000)
 #define end time (2:57, 177 seg) in miliseconds
 end_time=177
 
+#get the frame rate and the number of frames per second
 frame_rate = cap.get(cv2.CAP_PROP_FPS)
 frames_selected_time=int((end_time-start_time)*frame_rate)
 
+#declare lists and given values
 x_list=[]
 y_list=[]
 z_list=[]
@@ -120,6 +122,7 @@ cy=1080
 f=1723.72
 Z=50
 
+#Intiliase the figures for plot in 2d and 3d
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -127,7 +130,9 @@ ax = fig.add_subplot(111, projection='3d')
 fig_2 = plt.figure()
 pace= fig_2.add_subplot(111)
 
+
 def get_real_coordinates(x_obtained,y_obtained):
+        #Function that calculated the real coordinates
         coor_x,coor_y=x_obtained,y_obtained
 
         u=coor_x-cx
@@ -140,20 +145,25 @@ def get_real_coordinates(x_obtained,y_obtained):
         return x_global,y_global,z_global
     
 def plot_x_y_z(x_global,y_global,z_global):
+    #Function to plot the three real coordinates
     ax.plot(x_global, y_global, z_global,marker='o',color='blue')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
+    plt.title('X, Y, Z')
     plt.pause(0.1)
 
 
 def plot_pace(measurements,frames):
+    #function to plot the magnitude
     pace.plot(frames,measurements,marker='o',color='red')
-    pace.set_xlabel('X')
-    pace.set_ylabel('Y')
+    pace.set_xlabel('Magnitude of distance')
+    pace.set_ylabel('Frame')
+    plt.title('PACE')
     plt.pause(0.1)
 
 def get_acumulated_coordinates(x_global,y_global,z_global):
+    #appending coordinate values
     x_list.append(x_global)
     y_list.append(y_global)
     z_list.append(z_global)
@@ -161,11 +171,14 @@ def get_acumulated_coordinates(x_global,y_global,z_global):
     return x_list,y_list,z_list
 
 def draw_rectangle(contour):
+    #Getting the coordinates for the rectangle
     perimeter=cv2.arcLength(contour,True)
     polynomio=cv2.approxPolyDP(contour,0.02*perimeter,True)
     x_,y_,w,h=cv2.boundingRect(polynomio)
     
     return x_,y_,w,h
+
+#The following functions are to get the distance between two points in each coordinate
 
 def distance_in_x_from_center(x_list):
     substraction_in_x=x_list[-1]-x_list[-2]
@@ -186,23 +199,25 @@ def distance_in_z_from_center(z_list):
 
 
 def get_magnitude_of_distance(x_list,y_list,z_list):
+    #With the distance obtained, I obtained the magnitude
     measures_o_center_in_x=distance_in_x_from_center(x_list)
     measures_o_center_in_y=distance_in_y_from_center(y_list)
     measures_o_center_in_z=distance_in_z_from_center(z_list)
 
     magnitude=np.sqrt(np.square(measures_o_center_in_x)+np.square(measures_o_center_in_y)+np.square(measures_o_center_in_z))
-    
+    #I send the last value of the variable
     return magnitude[-1]
 
 def get_histogram_of_magnitude(list_of_magnitude):
-    
-    plt.hist(list_of_magnitude,bins=10,label=['Walked distance each frame'])
-    plt.pause(0.1)
+    #function to plot a histogram of the total of the magnitudes obtained
+    plt.hist(list_of_magnitude)
     plt.xlabel('Distance')
     plt.ylabel('frequency')
     plt.title('Histogram of distance')
+    plt.show()
 
 # ------- Main loop --------------- #
+#Using the total frames in the time interval
 for frames in range(frames_selected_time):
     # Read current frame
     ret, frame = cap.read()
@@ -226,26 +241,38 @@ for frames in range(frames_selected_time):
     contours, hierarchy = cv2.findContours(frame_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
     for contour in contours:
+        #finding the area of the contour detected
         area=cv2.contourArea(contour)
-        max_value=np.max(bitwise_AND[:,:,1])
-        if area>420:
+        #declaring a mininum of an area to draw the rectangle
+        if area>420:                    
             ax.clear
             pace.clear
+            #calling a function to determine the correct polynomio of the detected object and drawing the rectangle
             x_,y_,w,h=draw_rectangle(contour)
             cv2.rectangle(bitwise_AND,(x_-40,y_-40),(x_+100,y_+100),(0,255,0),1)
 
+            #Apending a one value of frame every 30 frames because we have a 30 fps
             if frames % (30) == 0:
                 frame_rate_list.append(frames)
+                #Calling a function that calculate the real coordinates with the equations provided and appending them in a list
                 x_global,y_global,z_global=get_real_coordinates(x_,y_)
                 x_list,y_list,z_list=get_acumulated_coordinates(x_global,y_global,z_global)
+                #plotting in 3D
                 plot_x_y_z(x_list,y_list,z_list)
             
+                #Wait until I have two elements in on my list of coordinates
                 if len(x_list)>=2:
+                    #calling a function that calculate the distance between two points and then get the magnitude of the three coordinates together
                     magnitude=(get_magnitude_of_distance(x_list,y_list,z_list))
+                    #appending the magnitudes in a list
                     measurements.append(magnitude)
+                    #getting the total distance with the sum  of all the values
                     accumulated_distance=sum(measurements)
-                    distance_in_plot_3d=fig.text(0,0,accumulated_distance,fontsize=10)
+                    #Add the value of the total distance in the 3d plot
+                    distance_in_plot_3d=fig.text(0,0,('accumulated distance:', accumulated_distance),fontsize=10)
+                    #calling a function to plot the distance traveled in each frame per second
                     plot_pace(measurements,frame_rate_list)
+                    #delete the total value to update it in the next frame
                     distance_in_plot_3d.remove()
                     
 
@@ -263,15 +290,14 @@ for frames in range(frames_selected_time):
         print("Programm finished, mate!")
         break
 
-fig_3 = plt.figure()
-histogram= fig_3.add_subplot(111)
-get_histogram_of_magnitude(measurements)
-
-#subplot to visualise the three plots
 
 # Destroy all visualisation windows
 cv2.destroyAllWindows()
-plt.show()
 
 # Destroy 'VideoCapture' object
 cap.release()
+
+#create subplot to call a histogram
+fig_3,histogram= plt.subplots()
+#calling a function to create a histogram with the list of all the magnitudes obtained
+get_histogram_of_magnitude(measurements)
